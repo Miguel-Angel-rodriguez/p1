2c743c6f22c0ae5c4461ee27f8445bc8dd663f1e
Miguel Ángel Rodríguez González <ma_r_g@yahoo.es>
2022-06-29 13:32:42 +0200

    Esto es nuevo

---
+++ src/index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Hola mundo de Git";
+
+
+$despe = new Adios("Felipe");
+
+$despe->despidir();
+
+$saludo2 = new Saludo("Santiago");
+$saludo2->saludar();
+
+$grito = new grito("Esto del git es una pasada");
+$grito->gritar();
+
+$saludo3 = new Saludo("Lucia");
+$saludo3->saludar();
+
